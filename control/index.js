26418d431ec344e98baf595f74af2dbff97e2733
ma-yuki <x.makino.yuki@gmail.com>
2020-05-08 10:28:23 +0900

    update control syntax

--- control/index.js
+++ control/index.js
@@ -1,7 +1,16 @@
-var app = new Vue({
-  el: '#app',
+var app3 = new Vue({
+  el: '#app3',
   data: {
     seen: true
   }
 })
-
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
